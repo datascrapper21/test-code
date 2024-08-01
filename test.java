6d8389dfcdd2fcde576f2bdc1b151237a2ac43ed
datascrapper21 <erc202009@gmail.com>
2024-08-01 17:47:44 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,10 @@
+class test{
+    public static void main(String[] args){
+    data2 cal = new data2();
+    System.out.println(cal.toMilitary());
+    cal.setTime(14, 23, 2);
+    System.out.println(cal.toMilitary()); 
+    System.out.println(cal.toString());
+    }
+    
+}
